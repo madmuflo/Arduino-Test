10ee1ebd70adc504f001f73276a7ba7086650d71
Alessandro Capretti <alessandro.capretti@wiloca.it>
2015-02-21 12:16:52 +0100

    Esempio 01 - Hello World

---
+++ sketch_helloWorld/sketch_helloWorld.ino
@@ -0,0 +1,25 @@
+/* HELLO WORLD!
+ * Programma che fa lampeggiare il led al pin13 (quello sulla scheda)
+ * Arduino UNO
+ */
+
+
+//definisco la variabile pin su cui andrà il mio output
+int ledPin = 13;
+
+//questa funzione lancia le impostazioni iniziali e viene eseguita una sola volta
+void setup() {
+  //dico al programma di indirizzare l'output sul pin definito prima
+  pinMode(ledPin,OUTPUT); 
+}
+
+//questa funzione viene eseguita di continuo (rappresenta il programma vero e proprio)
+void loop() {
+  //dico al pin di accendersi (porto la tensione a high level)
+  digitalWrite(ledPin, HIGH);
+  //attendo 1000ms
+  delay(1000);
+  //spengo il led portando la tensione a LOW
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
